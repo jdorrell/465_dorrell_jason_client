7f37438348cb4bffaef7332f8c5dc78bd00630b2
Jason Dorrell <j_dorrell0801@email.campbell.edu>
2017-09-29 17:30:44 -0400

    Initial develop commit

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-﻿var tls_client = require('./tls-client.js');
+﻿require('dotenv').config();
+
+var tls_client = require('./tls-client.js');
+    
+
+
+console.log("app.js Running in :" + process.env.NODE_ENV);
+function send(tls_client) {};
+
+send();
+
--- tls-client.js
+++ tls-client.js
@@ -1,8 +1,11 @@
 ﻿require('dotenv').config();
 
 var tls = require('tls'),
-    fs = require('fs');
-
+    fs = require('fs'),
+    test = require('./message.json');
+var modifiedTest = JSON.stringify(test);
+console.log(modifiedTest);    
+    
 var options = {
     host: process.env.SERVER_ADDRESS,
     port: process.env.SERVER_PORT,
@@ -11,23 +14,51 @@ var options = {
     ca: [fs.readFileSync(process.env.CA)]
 };
 
-var cleartextStream = tls.connect(options, function () {
-    console.log('connection to server', cleartextStream.authorized ? 'authorized' : 'unauthorized');
-    process.stdin.pipe(cleartextStream);
-    process.stdin.resume();
-});
+var message = process.env.MESSAGE;
+console.log(message);
 
-cleartextStream.setEncoding('utf8');
-cleartextStream.on('data', function (data) {
-    console.log(data);
-});
+function connection() {
 
-//cleartextStream.on('err', function (error) {
-//    console.log('server disconnected');
-//    //server.close;
-//});
+    var socket = tls.connect(options, function () {
+        console.log('tls_client: connection to server', socket.authorized ? 'authorized' : 'unauthorized');
+        process.stdin.pipe(socket);
+        process.stdin.resume();
+    });
 
-//cleartextStream.on('end', function (server) {
-    //console.log('server disconnected');
-    //server.close;
-//});
+    socket.setEncoding('utf8');
+    socket.write(modifiedTest);
+    socket.on('data', function (fromServer) {
+
+        console.log("tls_client has received:%s \n", fromServer);
+        
+        if (fromServer === modifiedTest) {
+            socket.write("line 2");
+        };
+
+        if (fromServer == "line 2") {
+            socket.write("line 3");
+        };
+
+        if (fromServer == "line 3") {
+            socket.write("line 4");
+        };
+
+        if (fromServer == "line 4") {
+            socket.write("socket closing");
+        };
+
+        if (fromServer == "socket closing") {
+            socket.destroy();
+        };
+
+    });
+
+    socket.once('close', function () {
+        console.log("socket closed");
+    });
+
+    
+};
+
+module.exports 
+    var  connect = connection();
