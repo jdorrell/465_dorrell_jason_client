8686546ac702a60decad86390e238764ff099c7a
Jason Dorrell <j_dorrell0801@email.campbell.edu>
2017-10-11 14:26:44 -0400

    Basic SMTP functionality - no data

--- app.js
+++ app.js
@@ -17,9 +17,6 @@ var options = {
 function connected(stream) {
 
     if (stream) {
-        // socket connected  
-        //process.stdin.pipe(stream);
-        //stream.write("helo");
     } else {
         console.log("Connection failed");
     }
@@ -29,16 +26,22 @@ function connected(stream) {
 
 // needed to keep socket variable in scope  
 var app = this;
+//var transactionCount = 0;
 
 // try to connect to the server  
 app.socket = tls.connect(options, function () {
     // callback called only after successful socket connection  
     app.connected = true;
     if (app.socket.authorized) {
-        // authorization successful  
+
+        // authorization successful 
+
+        //console.log(app.socket);//for testing
         app.socket.setEncoding('utf-8');
         connected(app.socket);
+
     } else {
+
         // authorization failed  
         console.log(app.socket.authorizationError);
         connected(null);
@@ -46,25 +49,30 @@ app.socket = tls.connect(options, function () {
 });
 
 app.socket.addListener('data', function (data) {
-    // received data  
-    //console.log(data);//start conversation here///////////////////////////////////////////
-    var res = data;
-    //transmit.send(res);
-    app.socket.write(transmit.send(res));
-    //////////////////////////////////if (data === process.env.SERVER_WELCOME) {
-    //////////////////////////////////    //console.log('yippeeeee!');
-    //////////////////////////////////    ///////////////////////////////////////call transmit here
-    //////////////////////////////////    var res = data;
-    //////////////////////////////////    app.socket.write(transmit.send(res));
-    //////////////////////////////////};
 
-    //////////////////////////////////if (data === "250 Hello client") {
-    //////////////////////////////////    //console.log('yippeeeee!');
-    //////////////////////////////////    ///////////////////////////////////////call transmit here
-    //////////////////////////////////    app.socket.write(transmit.send);
-    //////////////////////////////////};
-    
+    // received data  
+    var res = data;//server response
+    var req = transmit.send(res);//client request
+
+    console.log(res);
+
+    if (req === ('\r\n' + process.env.CLIENT_END + '\r\n')) {
+
+            app.socket.write("Way to go dumbass!");
+            app.socket.write(req);
+
+    } else 
+
+        if (req === res) {
+
+            app.socket.close;
+
+        } else
+
+            app.socket.write(req);
+
 });
+
 app.socket.addListener('error', function (error) {
     if (!app.connected) {
         // socket was not connected, notify callback  
@@ -78,29 +86,7 @@ app.socket.addListener('close', function () {
 });  
 
 
-
-
-//function connect() {
-//    tls.socket = tls.connect(options, function () {
-//        console.log('tls connection', tls.socket.authorized ? 'authorized' : 'unauthorized');
-//        process.stdin.pipe(tls.socket);
-//        process.stdin.resume();
-//    });
-//};
-
-//connect();
-
-//var connect = function () {
-//    tls.socket = tls.connect(options, function () {
-//        console.log('tls connection', tls.socket.authorized ? 'authorized' : 'unauthorized');
-//        process.stdin.pipe(tls.socket);
-//        process.stdin.resume();
-//    });
-//};
-
-//connect.on('data', function (data) {
-//    process.stdin.pipe(tls.socket);
-//    process.stdin.resume();
-//    tls.socket.write(data);
-//    console.log(data);
-//});
+//snippets
+//tls.socket.authorized ? 'authorized' : 'unauthorized');
+//process.stdin.pipe(callback);
+//process.stdin.resume(callback);
--- transmit.js
+++ transmit.js
@@ -1,17 +1,60 @@
 ﻿require('dotenv').config();
+require('./message.json');
+var transactionCount = 0;
 
-module.exports = {
-    send:   function (res) {
-                //console.log(res);
-                // return ('helo');
-                if (res === process.env.SERVER_WELCOME) {
-                    console.log(res);
-                    return (process.env.CLIENT_HELO);
-                };
 
-                if (res === process.env.SERVER_HELLO) {
-                    console.log(res);
-                    return ('handshake complete!!!');
-                };
-            }
-};
+module.exports =
+    {
+        send:
+        function (res) {
+            transactionCount ++
+            if (res === process.env.SERVER_WELCOME) {
+
+                //console.log('condition fullfilled');//for testting
+                return(process.env.CLIENT_HELO);
+
+            };
+
+            if (res === process.env.SERVER_HELLO && transactionCount === 2) {
+
+                //console.log('condition fullfilled');//for testting
+                return(process.env.MAIL_FROM + " booger");
+
+            };
+
+            if (res === process.env.SERVER_OK && transactionCount === 3) {
+
+                //console.log('condition fullfilled');//for testting
+                return(process.env.RCPT_TO);
+
+            };
+
+            if (res === process.env.SERVER_OK && transactionCount === 4) {
+
+                //console.log('condition fullfilled');//for testting
+                return(process.env.DATA);
+
+            };
+
+            if (res === process.env.SERVER_SEND && transactionCount === 5) {
+
+                //console.log('condition fullfilled');//for testting
+                return('\r\n' + process.env.CLIENT_END + '\r\n');
+
+            };
+
+            if (res === process.env.SERVER_OK + ", " + process.env.SERVER_ACCEPTS && transactionCount === 6) {
+
+                //console.log('condition fullfilled');//for testting
+                return(process.env.QUIT);
+            };//good to this line
+
+            if (res === process.env.SERVER_GOODBYE && transactionCount === 7) {
+
+                //console.log('condition fullfilled');//for testting
+                console.log("MESSAGE SENT!");
+                return (res);
+            };//good to this line
+        }
+    };
+//////////////////////////////////////////////////////////////////////////////////////////
